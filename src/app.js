b53b216dda5689f1b6043c6c5b1bfaa97bcdf5f6
Matteo Figus <matteofigus@gmail.com>
2017-01-18 13:45:23 +0000

    Read manifest (#6)
    * Read manifest
    
    * Cleanup
    
    * Cleanup
    
    * Using the correct mercury key for making reqs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const date = new Date();
-console.log(`Mercury running - ${date}`);
-process.exit(0);
+const _ 		= require('lodash');
+const async  	= require('async');
+const config 	= require('config');
+const github 	= require('./services/github');
+
+_.each(config.repositories, (repositories, owner) => {
+	async.each(repositories, (repo, next) => {
+
+		const options = {
+			apiToken: config.github.apiToken,
+			path: 'mercury.json',
+			repo,
+			owner
+		};
+
+		github.getFileContent(options, (err, result) => {
+			console.log(`got following result for ${owner}/${repo}:`);
+			console.log(err || result);
+			next(err);
+		});
+		
+	}, (err) => {
+		const date = new Date();
+		console.log(`Mercury just ran - ${date}`);
+		process.exit(err ? 1 : 0);
+	});
+});
---
+++ src/services/github.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let github = new require('github')({
+    protocol: 'https',
+    host: 'api.github.com',
+    headers: {
+        'user-agent': 'mercury'
+    },
+    followRedirects: false,
+    timeout: 5000
+});
+
+module.exports = {
+    getFileContent: (options, next) => {
+        github.authenticate({
+            type: 'oauth',
+            token: options.apiToken
+        });
+
+        github.repos.getContent(options, (err, file) => {
+            const getContent = f => new Buffer(f.content, f.encoding).toString();
+            const content = !err && file ? getContent(file) : undefined;
+            return next(err, content);
+        });
+    }
+};
