abe4981eb814462c9fd19e0ce134ce5f7024f1a9
Federico Maffei <federico.maffei@gmail.com>
2017-02-17 10:20:24 +0000

    wrong rebase fixed

--- src/services/github.js
+++ src/services/github.js
@@ -79,7 +79,7 @@ const getReference = (options, referencePath, next) => {
 
 const getMasterReference = (options, next) => getReference(options, 'heads/master', next);
 
-const getBranchReference = (options, next) => getReference(options, 'heads/mercury', next);
+const getBranchReference = (options, next) => getReference(options, `heads/${options.branch}`, next);
 
 const ensureBranchReference = (options, sourceSha, next) => {
 
@@ -89,7 +89,7 @@ const ensureBranchReference = (options, sourceSha, next) => {
             return next(err, branchReferenceSha);
         }
         
-        options.ref = 'refs/heads/mercury';
+        options.ref = `refs/heads/${options.branch}`;
         options.sha = sourceSha;
 
         github.gitdata.createReference(options, (err, branchReference) => {
