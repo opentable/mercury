3ff111124aa71b06acfdc266f1006654271558d2
Federico Maffei <federico.maffei@gmail.com>
2017-02-17 16:35:04 +0000

    refactored out forking operation, with tests

---
+++ src/resources/ensure-fork.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const errorTypes = require('../constants/error-types');
+const github = require('../services/github');
+const Logger = require('../services/logger-service');
+
+const loggerService = Logger();
+
+module.exports = (repository, callback) => {
+
+    loggerService.info(`Ensuring existence of a Mercury fork for ${repository.owner}/${repository.repo}`);
+    
+    const forkOptions = {
+        owner: repository.owner,
+        repo: repository.repo
+    };
+        
+    github.ensureFork(forkOptions, (err, result) => {
+        
+        if(err){
+            loggerService.error(err, errorTypes.failedGithubFork, repository);
+            repository.skip = true;
+        }
+        
+        repository.mercuryForkName = result && result.full_name ? result.full_name : null;
+        repository.mercuryForkOwner = result && result.owner ? result.owner.login: null;
+
+        callback(err, repository);
+    });
+};
