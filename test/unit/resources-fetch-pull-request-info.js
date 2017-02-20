e6503b197329408e0a69bb9737c57cd286a867a8
Matteo Figus <matteofigus@gmail.com>
2017-02-20 14:13:17 +0000

    Spaces vs tabs II

--- test/unit/resources-fetch-pull-request-info.js
+++ test/unit/resources-fetch-pull-request-info.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const _         = require('lodash');
-const expect 	= require('chai').expect;
+const _			= require('lodash');
+const expect	= require('chai').expect;
 const injectr	= require('injectr');
 const sinon		= require('sinon');
-const testData  = require('./testData');
+const testData	= require('./testData');
 
 describe('resources.fetchPullRequestInfo()', () => {
 	
