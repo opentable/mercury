30225ed81d81cdc3b8454a3ac5691eea999394fd
federicomaffei <federico.maffei@gmail.com>
2017-03-23 08:27:05 +0000

    little cleanup, tests green

--- src/resources/commit-files.js
+++ src/resources/commit-files.js
@@ -12,10 +12,10 @@ const loggerService = Logger();
 module.exports = (repository, callback) => {
 
     loggerService.info(`Committing new or updated files to ${repository.mercuryForkOwner}/${repository.repo}`);
-    
+
     async.eachSeries(repository.translationFiles, (file, callback) => {
         async.eachOfSeries(file.locales, (locale, localeId, callback) => {
-                        
+
             const options = {
                 owner: config.github.owner,
                 repo: repository.repo,
@@ -28,25 +28,25 @@ module.exports = (repository, callback) => {
             if(locale.isDifferent) {
                 github.getFile(options, (err, file) => {
                     if(err && err.code !== 404){
-                        return callback(new Error(err.message));    
+                        return callback(new Error(err.message));
                     }
-                    
+
                     const content = file.content;
                     const sha = file.sha;
-                    
+
                     _.set(options, 'branch', options.ref);
                     _.unset(options, 'ref');
-                    
+
                     if(!content) {
                         loggerService.info(`Creating new ${localeId} file ${locale.githubPath} on ${repository.mercuryForkOwner}/${repository.repo}`);
-                        
+
                         async.retry({
-                            times: 10,
+                            times: 5,
                             interval: function(retryCount) {
-                                return 100 * retryCount;
+                                return 200 * retryCount;
                             }
                         }, github.createFile.bind(null, options), callback);
-                        
+
                     } else if(content && content !== locale.smartlingContent) {
                         loggerService.info(`Updating existing ${localeId} file ${locale.githubPath} on ${repository.mercuryForkOwner}/${repository.repo}`);
                         options.sha = sha;
@@ -58,16 +58,16 @@ module.exports = (repository, callback) => {
             } else {
                 callback();
             }
-                                    
+
         }, callback);
     }, (err) => {
-                                    
+
         if(err){
             err = new Error(err.message);
             loggerService.error(err, errorTypes.failedGithubCommit, repository);
             repository.skip = true;
         }
-        
+
         callback(err, repository);
     });
 };
