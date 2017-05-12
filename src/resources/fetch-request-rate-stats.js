c8e97b17f16477b68ebe808ac60f49add94e88dc
Federico Maffei <federico.maffei@gmail.com>
2017-05-12 14:21:48 +0100

    removing annoying mixed tabs and spaces

--- src/resources/fetch-request-rate-stats.js
+++ src/resources/fetch-request-rate-stats.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const errorTypes 	= require('../constants/error-types');
-const github 		= require('../services/github');
-const Logger 		= require('../services/logger-service');
+const errorTypes     = require('../constants/error-types');
+const github         = require('../services/github');
+const Logger         = require('../services/logger-service');
 
 const loggerService = Logger();
 
 module.exports = (callback) => {
 
-	loggerService.info(`Getting GitHub hourly rate limit status`);
+    loggerService.info(`Getting GitHub hourly rate limit status`);
     
-	github.getRequestRateStats((err, stats) => {
+    github.getRequestRateStats((err, stats) => {
         
-		if(err){
-			err = new Error('Failed while fetching GitHub hourly rate limit status');
-		} else {
+        if(err){
+            err = new Error('Failed while fetching GitHub hourly rate limit status');
+        } else {
             
             const requestsLimit = stats.resources.core.limit;
             const requestsRemaining = stats.resources.core.remaining;
             const requestsSent = requestsLimit - requestsRemaining;
             
             loggerService.info(`Requests sent in this hour: ${requestsSent}`);
-			loggerService.info(`Remaining requests for the hour: ${requestsRemaining}`);
-		}
+            loggerService.info(`Remaining requests for the hour: ${requestsRemaining}`);
+        }
 
-		callback();
-	});
+        callback();
+    });
 };
