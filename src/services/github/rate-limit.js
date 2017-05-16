5de742b0ab095c7e05cfa67f8aa223de3d5f78f4
federicomaffei <federico.maffei@gmail.com>
2017-05-16 08:44:32 +0100

    better stats for bot limits

--- src/services/github/rate-limit.js
+++ src/services/github/rate-limit.js
@@ -1,13 +1,66 @@
 'use strict';
 
-const utils  = require('./utils');
+const async  = require('async');
+const config = require('config');
 
 module.exports = (github) => ({
 
     get: (next) => {
-        
-        const authenticatedGithub = utils.authenticateGithubOperation('write', github);
-        
-        authenticatedGithub.misc.getRateLimit({}, next);
+
+        const stats = {};
+        const options = {};
+
+        async.eachOfSeries(config.github.readApiTokens, (token, index, next) => {
+
+            github.authenticate({
+                type: 'oauth',
+                token
+            });
+
+            github.misc.getRateLimit(options, (err, rateLimit) => {
+
+                if(err) {
+                    return next(err);
+                }
+
+                const requestsLimit = rateLimit.resources.core.limit;
+                const requestsRemaining = rateLimit.resources.core.remaining;
+                const requestsSent = requestsLimit - requestsRemaining;
+
+                stats[`mercury-bot-${index + 1}-limit`] = requestsLimit;
+                stats[`mercury-bot-${index + 1}-remaining`] = requestsRemaining;
+                stats[`mercury-bot-${index + 1}-sent`] = requestsSent;
+
+                next();
+            });
+
+        }, (err) => {
+
+            if(err) {
+                return next(err);
+            }
+
+            github.authenticate({
+                type: 'oauth',
+                token: config.github.writeApiToken
+            });
+
+            github.misc.getRateLimit(options, (err, rateLimit) => {
+
+                if(err) {
+                    return next(err);
+                }
+
+                const requestsLimit = rateLimit.resources.core.limit;
+                const requestsRemaining = rateLimit.resources.core.remaining;
+                const requestsSent = requestsLimit - requestsRemaining;
+
+                stats[`mercury-write-limit`] = requestsLimit;
+                stats[`mercury-write-remaining`] = requestsRemaining;
+                stats[`mercury-write-sent`] = requestsSent;
+
+                next(err, stats);
+            });
+        });
     }
 });
